facef06e16772aab9d3d41a2b7ce30d94841757f
flavia <flavia.alvarez@pedidosya.com>
2020-10-29 19:48:36 -0300

    change Message class

--- social_network.py
+++ social_network.py
@@ -2,9 +2,9 @@ import datetime
 
 class Timeline:
     def __init__(self):
-        self.posts = []
+        self.messages = []
 
-class message:
+class Message:
     def __init__(self, content, user_name):
         self.user_name = user_name
         self.content = content
@@ -17,14 +17,14 @@ class User:
         self.timeline = timeline
         self.following = []
 
-    def post_comment(self, comment):
-        self.timeline.posts.append(message(comment, self.name))
+    def post_message(self, content):
+        self.timeline.messages.append(Message(content, self.name))
 
     def add_following(self, user):
        self.following.append(user)
-       self.post_comment(f'has started following {user}')
+       self.post_message(f'has started following {user}')
 
     def get_timeline(self):
         user_list = self.following
         user_list.append(self.name)
-        return [x.user_name + ': '+ x.content for x in self.timeline.posts if x.user_name in user_list]
+        return [x.user_name + ': '+ x.content for x in self.timeline.messages if x.user_name in user_list]
--- unit_test.py
+++ unit_test.py
@@ -1,26 +1,27 @@
 from unittest import TestCase
 
-from social_network import User, Timeline
+from social_network import User, Timeline, Message
 
 
 class TestUser(TestCase):
     def test_alice_post_comment(self):
-        Alice = User(1, 'Alice')
-        Alice.post_comment('Esto es una prueba')
-        Alice.post_comment('Esto es otra prueba')
-        posts = [Alice.timeline.posts[0].content]
-        posts.append(Alice.timeline.posts[1].content)
-        self.assertEqual(posts, ['Esto es una prueba', 'Esto es otra prueba'])
+        timeline = Timeline()
+        Alice = User(1, 'Alice', timeline)
+        Alice.post_message('Esto es una prueba')
+        Alice.post_message('Esto es otra prueba')
+        messages = [timeline.messages[0].content]
+        messages.append(timeline.messages[1].content)
+        self.assertEqual(messages, ['Esto es una prueba', 'Esto es otra prueba'])
 
     def test_bob_can_view_alice_timeline(self):
         timeline = Timeline()
         Alice = User(1, 'Alice', timeline)
-        Alice.post_comment('Esto es una prueba')
+        Alice.post_message('Esto es una prueba')
         Bob = User(2, 'Bob', timeline)
-        Alice.post_comment('Esto es otra prueba')
-        posts = [Bob.timeline.posts[0].content]
-        posts.append(Bob.timeline.posts[1].content)
-        self.assertEqual(posts, ['Esto es una prueba', 'Esto es otra prueba'])
+        Alice.post_message('Esto es otra prueba')
+        messages = [Bob.timeline.messages[0].content]
+        messages.append(Bob.timeline.messages[1].content)
+        self.assertEqual(messages, ['Esto es una prueba', 'Esto es otra prueba'])
 
     def test_charlie_follows_alice_and_bob(self):
         timeline = Timeline()
@@ -40,9 +41,9 @@ class TestUser(TestCase):
 
         Charlie.add_following(Alice.name)
 
-        Alice.post_comment('Esto es una prueba')
-        Alice.post_comment('Esto es otra prueba')
-        Bob.post_comment('Esto no se tiene que ver')
+        Alice.post_message('Esto es una prueba')
+        Alice.post_message('Esto es otra prueba')
+        Bob.post_message('Esto no se tiene que ver')
 
         self.assertEqual(Charlie.get_timeline(), ['Charlie: has started following Alice', 'Alice: Esto es una prueba', 'Alice: Esto es otra prueba'])
 
@@ -53,13 +54,13 @@ class TestUser(TestCase):
         Bob = User(2, 'Bob', timeline)
         Charlie = User(3, 'Charlie', timeline)
 
-        Charlie.post_comment('el numero cero')
+        Charlie.post_message('el numero cero')
         Charlie.add_following(Bob.name)
 
-        Alice.post_comment('primero')
-        Bob.post_comment('segundo')
-        Alice.post_comment('tercero')
-        Bob.post_comment('cuarto')
+        Alice.post_message('primero')
+        Bob.post_message('segundo')
+        Alice.post_message('tercero')
+        Bob.post_message('cuarto')
 
         self.assertEqual(Charlie.get_timeline(), ['Charlie: el numero cero', 'Charlie: has started following Bob', 'Bob: segundo', 'Bob: cuarto'])
 
@@ -74,9 +75,9 @@ class TestUser(TestCase):
         Charlie.add_following(Alice.name)
         Charlie.add_following(Bob.name)
 
-        Alice.post_comment('primero')
-        Bob.post_comment('segundo')
-        Alice.post_comment('tercero')
+        Alice.post_message('primero')
+        Bob.post_message('segundo')
+        Alice.post_message('tercero')
 
         self.assertEqual(Charlie.get_timeline(), ['Charlie: has started following Alice', 'Charlie: has started following Bob',
                                                           'Alice: primero', 'Bob: segundo', 'Alice: tercero'])
